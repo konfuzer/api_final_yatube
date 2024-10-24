3dde04bca24d639a57656bd83f25ede76d06c42c
Ivan <ivan.artemov.04@gmail.com>
2024-10-24 19:42:12 +0300

    INIT

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -21,7 +21,11 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+        Group,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts')
 
     def __str__(self):
         return self.text
